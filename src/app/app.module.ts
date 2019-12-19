35d405de8d3a3228615a2aa1740b0d755f45ec65
Avala <vinod.avala@epsilon.com>
2019-12-19 14:46:24 +0530

    Angular changes

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IntroComponent } from './intro/intro.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContentComponent } from './content/content.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { FooterComponent } from './footer/footer.component';
+import { ClientsComponent } from './clients/clients.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { HeaderComponent } from './header/header.component';
+import { SocialComponent } from './social/social.component';
+import { NavigationComponent } from './navigation/navigation.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IntroComponent,
+    GalleryComponent,
+    ContentComponent,
+    TestimonialsComponent,
+    FooterComponent,
+    ClientsComponent,
+    PricingComponent,
+    HeaderComponent,
+    SocialComponent,
+    NavigationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
